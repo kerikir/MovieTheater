7b3f3ba54678f9845a5beeb1002382340b7e46b8
kerikir <vlasov-kerik@mail.ru>
2026-07-14 17:18:41 +0300

    added font family

--- app/src/main/java/com/kerikir/movietheater/ui/theme/Type.kt
+++ app/src/main/java/com/kerikir/movietheater/ui/theme/Type.kt
@@ -2,9 +2,19 @@ package com.kerikir.movietheater.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.kerikir.movietheater.R
+
+
+private val RobotoFontFamily = FontFamily(
+    Font(R.font.font_roboto_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.font_roboto_black, FontWeight.Black, FontStyle.Normal),
+    Font(R.font.font_roboto_regular, FontWeight.Normal, FontStyle.Normal)
+)
 
 
 val Typography = Typography(
