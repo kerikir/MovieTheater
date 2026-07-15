3577f03eb0b28418a6c2c5359905e884c564e0fd
kerikir <vlasov-kerik@mail.ru>
2026-07-15 15:15:07 +0300

    added a composable function for the discovery screen

---
+++ app/src/main/java/com/kerikir/movietheater/ui/screen/DiscoveryScreen.kt
@@ -0,0 +1,10 @@
+package com.kerikir.movietheater.ui.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun DiscoveryScreen(
+    modifier: Modifier = Modifier
+) {}
