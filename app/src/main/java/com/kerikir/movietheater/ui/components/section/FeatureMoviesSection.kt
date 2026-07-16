8208ad49ba8cbaa9cc6401d39e515b82d1056fa1
kerikir <vlasov-kerik@mail.ru>
2026-07-16 14:14:57 +0300

    added composable function for full-length movies

---
+++ app/src/main/java/com/kerikir/movietheater/ui/components/section/FeatureMoviesSection.kt
@@ -0,0 +1,12 @@
+package com.kerikir.movietheater.ui.components.section
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.kerikir.movietheater.data.FeaturedMoviesState
+
+
+@Composable
+fun FeatureMoviesSection(
+    modifier: Modifier = Modifier,
+    data: List<FeaturedMoviesState>
+) {}
