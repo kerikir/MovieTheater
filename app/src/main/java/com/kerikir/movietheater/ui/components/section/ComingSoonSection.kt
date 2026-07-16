e591d799306f357d8e6334a710e48d1d90983c31
kerikir <vlasov-kerik@mail.ru>
2026-07-16 17:37:14 +0300

    added a title for the upcoming movies section

--- app/src/main/java/com/kerikir/movietheater/ui/components/section/ComingSoonSection.kt
+++ app/src/main/java/com/kerikir/movietheater/ui/components/section/ComingSoonSection.kt
@@ -1,12 +1,25 @@
 package com.kerikir.movietheater.ui.components.section
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.kerikir.movietheater.data.MovieThumbnailState
+import com.kerikir.movietheater.ui.components.SectionHeader
 
 
 @Composable
 fun ComingSoonSection(
     modifier: Modifier = Modifier,
     data: List<MovieThumbnailState>
-) {}
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        SectionHeader(
+            text = "Coming Soon"
+        )
+    }
+}
