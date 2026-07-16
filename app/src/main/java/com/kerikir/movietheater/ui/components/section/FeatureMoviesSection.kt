a10b72720c306311dd18152fbc6ab632b1a1a6cb
kerikir <vlasov-kerik@mail.ru>
2026-07-16 14:53:25 +0300

    added a title for recommended movies

--- app/src/main/java/com/kerikir/movietheater/ui/components/section/FeatureMoviesSection.kt
+++ app/src/main/java/com/kerikir/movietheater/ui/components/section/FeatureMoviesSection.kt
@@ -9,10 +9,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.kerikir.movietheater.data.FeaturedMovieState
 import com.kerikir.movietheater.ui.components.SectionHeader
@@ -62,6 +65,12 @@ private fun FeaturedMovie(
             modifier = Modifier
                 .fillMaxWidth()
                 .height(324.dp)
+        )
+        Text(
+            text = item.title,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            style = MaterialTheme.typography.titleMedium
         )
     }
 }
