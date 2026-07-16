5e74e9eacf634fa0892f5131aa5421f6def9026b
kerikir <vlasov-kerik@mail.ru>
2026-07-16 16:55:02 +0300

    added a composable function for the upcoming movies section

---
+++ app/src/main/java/com/kerikir/movietheater/ui/components/section/ComingSoonSection.kt
@@ -0,0 +1,10 @@
+package com.kerikir.movietheater.ui.components.section
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun ComingSoonSection(
+    modifier: Modifier = Modifier
+) {}
