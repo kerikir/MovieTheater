b55fcf4ee09f1f94031a595e7e4073272052ab3a
kerikir <vlasov-kerik@mail.ru>
2026-07-16 14:47:28 +0300

    added a movie poster to the composable function for the recommended movie

--- app/src/main/java/com/kerikir/movietheater/ui/components/section/FeatureMoviesSection.kt
+++ app/src/main/java/com/kerikir/movietheater/ui/components/section/FeatureMoviesSection.kt
@@ -1,9 +1,16 @@
 package com.kerikir.movietheater.ui.components.section
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.kerikir.movietheater.data.FeaturedMovieState
 import com.kerikir.movietheater.ui.components.SectionHeader
@@ -20,17 +27,36 @@ fun FeatureMoviesSection(
         SectionHeader(
             text = "Featured Movies"
         )
+        LazyRow {
+            items(
+                items = data,
+                key = { it.id }
+            ) { item ->
+                FeaturedMovie(
+                    item = item
+                )
+            }
+        }
     }
 }
 
 
 
 @Composable
-private fun FeaturedMovies(
+private fun FeaturedMovie(
     modifier: Modifier = Modifier,
     item: FeaturedMovieState
 ) {
     Column(
         modifier= modifier.width(224.dp)
-    ) { }
+    ) {
+        Image(
+            painter = painterResource(item.img),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(324.dp)
+        )
+    }
 }
