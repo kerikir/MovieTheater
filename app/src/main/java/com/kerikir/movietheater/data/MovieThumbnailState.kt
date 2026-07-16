12022885197df563aa2850c1e90ff6841542cbda
kerikir <vlasov-kerik@mail.ru>
2026-07-16 16:59:29 +0300

    added data to fill in for movies that are coming out soon

--- app/src/main/java/com/kerikir/movietheater/data/MovieThumbnailState.kt
+++ app/src/main/java/com/kerikir/movietheater/data/MovieThumbnailState.kt
@@ -1,8 +1,31 @@
 package com.kerikir.movietheater.data
 
 import androidx.annotation.DrawableRes
+import com.kerikir.movietheater.R
+
 
 data class MovieThumbnailState(
     val id: Int,
     @DrawableRes val img: Int
+)
+
+
+
+val UpcomingMoviesData = listOf(
+    MovieThumbnailState(
+        id = 0,
+        img = R.drawable.img_movie_poster_3
+    ),
+    MovieThumbnailState(
+        id = 1,
+        img = R.drawable.img_movie_poster_1
+    ),
+    MovieThumbnailState(
+        id = 2,
+        img = R.drawable.img_movie_poster_0
+    ),
+    MovieThumbnailState(
+        id = 3,
+        img = R.drawable.img_movie_poster_8
+    )
 )
