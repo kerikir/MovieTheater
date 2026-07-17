a7bbc300ed234e894e221b19a92414c3e64d199c
kerikir <vlasov-kerik@mail.ru>
2026-07-17 16:42:29 +0300

    added scroll to the screen

--- app/src/main/java/com/kerikir/movietheater/ui/screen/DiscoveryScreen.kt
+++ app/src/main/java/com/kerikir/movietheater/ui/screen/DiscoveryScreen.kt
@@ -2,6 +2,8 @@ package com.kerikir.movietheater.ui.screen
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.kerikir.movietheater.data.DiscoverScreenState
@@ -14,8 +16,12 @@ fun DiscoveryScreen(
     modifier: Modifier = Modifier,
     screenState: DiscoverScreenState
 ) {
+    val scrollableState = rememberScrollState()
+
     Column(
-        modifier = modifier.fillMaxSize()
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(scrollableState)
     ) {
         FeatureMoviesSection(
             data = screenState.featureMovies
