cc933c3e8f2257fddd9b39c4397bd09bdc823cdb
kerikir <vlasov-kerik@mail.ru>
2026-07-17 17:05:56 +0300

    added a header for the horizontal section

--- app/src/main/java/com/kerikir/movietheater/ui/components/section/HorizontalSection.kt
+++ app/src/main/java/com/kerikir/movietheater/ui/components/section/HorizontalSection.kt
@@ -1,8 +1,13 @@
 package com.kerikir.movietheater.ui.components.section
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.kerikir.movietheater.data.MovieThumbnailState
+import com.kerikir.movietheater.ui.components.SectionHeader
 
 
 @Composable
@@ -10,4 +15,14 @@ fun HorizontalSection(
     modifier: Modifier = Modifier,
     data: List<MovieThumbnailState>,
     name: String
-) {}
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        SectionHeader(
+            text = name,
+            modifier = Modifier.padding(horizontal = 18.dp)
+        )
+    }
+}
