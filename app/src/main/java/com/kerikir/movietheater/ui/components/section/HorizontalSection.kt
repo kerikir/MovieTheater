6841bc1e2bd4d99526cc2f1070dd7c580e1abea4
kerikir <vlasov-kerik@mail.ru>
2026-07-17 17:03:51 +0300

    added a composable function for the horizontal section

---
+++ app/src/main/java/com/kerikir/movietheater/ui/components/section/HorizontalSection.kt
@@ -0,0 +1,13 @@
+package com.kerikir.movietheater.ui.components.section
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.kerikir.movietheater.data.MovieThumbnailState
+
+
+@Composable
+fun HorizontalSection(
+    modifier: Modifier = Modifier,
+    data: List<MovieThumbnailState>,
+    name: String
+) {}
