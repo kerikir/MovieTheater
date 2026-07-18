84a710664ead4d1272e6af25a423135b5b2d3eea
kerikir <vlasov-kerik@mail.ru>
2026-07-18 15:06:28 +0300

    updated screen state

--- app/src/main/java/com/kerikir/movietheater/data/DiscoverScreenState.kt
+++ app/src/main/java/com/kerikir/movietheater/data/DiscoverScreenState.kt
@@ -2,5 +2,7 @@ package com.kerikir.movietheater.data
 
 data class DiscoverScreenState(
     val featureMovies: List<FeaturedMovieState> = FeatureMoviesData,
-    val upcomingMovies: List<MovieThumbnailState> = UpcomingMoviesData
+    val upcomingMovies: List<MovieThumbnailState> = UpcomingMoviesData,
+    val recentlyWatchedMovies: List<MovieThumbnailState> = RecentlyWatchedMoviesData,
+    val streamingMovies: List<MovieThumbnailState> = StreamingMoviesData
 )
