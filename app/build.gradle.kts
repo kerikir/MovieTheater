df3b0e08013ccb86705c05bbd818100149e57e65
kerikir <vlasov-kerik@mail.ru>
2026-07-18 15:52:31 +0300

    updated sdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 android {
     namespace = "com.kerikir.movietheater"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.kerikir.movietheater"
         minSdk = 28
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
     }
